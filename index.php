d761c984d44560036107609f550d4abc36febac6
Misy <78818724+zrfli@users.noreply.github.com>
2022-06-11 14:52:20 +0300

    Misy Web

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Misy</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="assets//apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets//favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets//favicon-16x16.png">
+    <link rel="manifest" href="assets//site.webmanifest">    
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Misy" name="keywords" />
+    <meta content="Misy" name="author" />
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />    
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body style="height:100vh">
+
+    <!-- section begin -->
+    <section id="section-hero" aria-label="section" class="bg-loop text-light overflow-hidden" data-bgimage="url(https://gigaland.io/images/background/22.jpg) top" style="height:100%" align="center">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="spacer-double"></div>
+                    <div class="text_top">
+                        <h1>Next generation software solutions in international standards</h1>
+                        <div class="d-flex justify-content-center">
+                            <p>Contact us : <u href="mailto:hi@misy.dev" class="text-light" onclick="window.location.href='mailto:hi@misy.dev?subject=Misy Web #<?= rand(111111,999999); ?>';">hi@misy.dev</u></p>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <p><strong>Powered by :</strong> Misy</p>
+                        </div>
+                    </div>
+                </div>
+            </div>                    
+        </div>
+    </section>
+    <!-- section close -->
+
+    <!-- Javascript Files -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/designesia.js"></script>
+
+</body>
+</html>
